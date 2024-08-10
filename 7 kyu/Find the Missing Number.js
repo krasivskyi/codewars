4335a97cb89e7a6b02f51b7a14aaac3e19816223
LiubomyrKr <lubass23@gmail.com>
2024-08-10 17:57:23 +0300

    Find the Missing Number

---
+++ 7 kyu/Find the Missing Number.js
@@ -0,0 +1,11 @@
+/*The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+
+You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+
+https://www.codewars.com/kata/57f5e7bd60d0a0cfd900032d */
+
+function missingNo(nums) {
+  const expectedSum = (100 * (100 + 1)) / 2;
+  const actualSum = nums.reduce((sum, num) => sum + num, 0);
+  return expectedSum - actualSum;
+}
