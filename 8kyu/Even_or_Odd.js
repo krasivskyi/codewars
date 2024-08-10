0353c4919bd0311bf494875a78f1c77d435d6df5
LiubomyrKr <lubass23@gmail.com>
2024-08-10 18:54:16 +0300

    Even or Odd + Find min and max

---
+++ 8kyu/Even_or_Odd.js
@@ -0,0 +1,8 @@
+/* https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+function evenOrOdd(number) {
+  return number % 2 ? "Odd" : "Even";
+}
+
+console.log(evenOrOdd(4));
