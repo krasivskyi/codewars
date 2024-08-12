f830b225cccf119eb43f177d179b4f373fa14a64
LiubomyrKr <lubass23@gmail.com>
2024-08-12 12:44:19 +0300

    Sum of positive

--- 7kyu/Shortest_Word.js
+++ 7kyu/Shortest_Word.js
@@ -5,6 +5,6 @@ Simple, given a string of words, return the length of the shortest word(s).
 String will never be empty and you do not need to account for different data types. */
 
 function findShort(s) {
-    return Math.min(...s.split(" ").map((x) => x.length));
+  return Math.min(...s.split(" ").map((x) => x.length));
 }
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
---
+++ 8kyu/Sum_of_positive.js
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/sum-of-positive
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
+
+function positiveSum(arr) {
+    return arr.filter((x) => x > 0).reduce((sum, x) => sum + x, 0);
+}
