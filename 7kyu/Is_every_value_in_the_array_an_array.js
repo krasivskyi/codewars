029a5214bcf5c82a9d1c8880d556d13e26d9d48e
LiubomyrKr <lubass23@gmail.com>
2024-08-14 20:48:38 +0300

    Is every value in the array an array?

---
+++ 7kyu/Is_every_value_in_the_array_an_array.js
@@ -0,0 +1,16 @@
+/* Is every value in the array an array?
+
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+Examples:
+
+[[1],[2]] => true
+['1','2'] => false
+[{1:1},{2:2}] => false */
+const arrCheck = value => Array.isArray(value);
+
+
+//other solution
+    function arrCheck2(arr) {
+  return arr.every(element => Array.isArray(element));
+}
